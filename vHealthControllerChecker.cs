0c343545da06fb789ee57e6c4b6d37dff0e62b02
tairasoul <100938045+tairasoul@users.noreply.github.com>
2024-01-12 00:15:15 +0200

    Add project files.

---
+++ vHealthControllerChecker.cs
@@ -0,0 +1,16 @@
+﻿using Invector;
+using UnityEngine;
+
+namespace HealthUpgrade
+{
+    internal class vHealthControllerChecker : MonoBehaviour
+    {
+        void Update()
+        {
+            if (GameObject.Find("S-105") != null)
+            {
+                Plugin.Instance.healthController = GameObject.Find("S-105").GetComponent<vHealthController>();
+            }
+        }
+    }
+}
