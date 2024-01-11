54f34e13dbb6ab98d04a28c2a2e69464fdd9982a
tairasoul <mjrcoetzee+1@outlook.com>
2024-01-12 00:15:15 +0200

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,84 @@
+﻿using BepInEx;
+using Invector;
+using System.Collections;
+using UnityEngine;
+using UpgradeFramework;
+
+namespace HealthUpgrade
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "tairasoul.healthupgrade";
+        public const string Name = "HealthUpgrade";
+        public const string Version = "1.0.0";
+    }
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    internal class Plugin : BaseUnityPlugin
+    {
+        internal static Plugin Instance;
+        internal vHealthController healthController;
+        internal bool init = false;
+        void Awake()
+        {
+            Instance = this;
+            Logger.LogInfo($"Plugin {PluginInfo.GUID} ({PluginInfo.Name}) version {PluginInfo.Version} loaded.");
+        }
+
+        int[] levelPrices = [100, 200, 300, 400, 500, 600];
+        int[] refundPrices = [75, 150, 225, 300, 375, 450];
+
+        int[] Upgraded(int level, int currentPrice)
+        {
+            Logger.LogInfo(level);
+            Logger.LogInfo(levelPrices[level - 1]);
+            Logger.LogInfo(refundPrices[level - 1]);
+            bool increaseCurrent = false;
+            if (healthController?.currentHealth == healthController?.maxHealth)
+                increaseCurrent = true;
+            if (healthController != null) healthController.maxHealth = level;
+            if (increaseCurrent && healthController != null)
+                healthController.currentHealth = level;
+            return [levelPrices[level - 1], refundPrices[level - 1]];
+        }
+
+        int[] Downgraded(int level, int currentPrice)
+        {
+            Logger.LogInfo(level);
+            Logger.LogInfo(levelPrices[level - 1]);
+            Logger.LogInfo(refundPrices[level - 1]);
+            if (healthController != null) healthController.maxHealth = level;
+            if (healthController?.currentHealth > level && healthController != null)
+                healthController.currentHealth = level;
+            return [levelPrices[level - 1], refundPrices[level - 1]];
+        }
+
+        void Start() => Init();
+        void OnDestroy() => Init();
+
+        void Init()
+        {
+            if (init) return;
+            init = true;
+            GameObject upg = new("HealthUpgrade");
+            DontDestroyOnLoad(upg);
+            upg.AddComponent<vHealthControllerChecker>().StartCoroutine(InitCoroutine());
+        }
+
+        IEnumerator InitCoroutine()
+        {
+            while (!Framework.Ready)
+            {
+                yield return null;
+            }
+            try
+            {
+                Upgrade upgrade = new("Health", "healthupgrade.health", "Frame", "Toughness of Sen's frame.", Upgraded, Downgraded, 3, 1, 6, levelPrices[2], refundPrices[2]);
+                Framework.AddUpgrade(upgrade);
+            }
+            catch (System.Exception ex)
+            {
+                Logger.LogError(ex);
+            }
+        }
+    }
+}
